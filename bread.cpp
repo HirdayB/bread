6a4cdab2e07742d2d1064031b6b2f89a08112e83
HirdayB <s3901303@student.rmit.edu.au>
2024-12-05 21:48:37 +1100

    hasbread method

---
+++ bread.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+bool hasBread(std::string text){
+    std::string bread = "bread";
+    if (text.find(bread) != std::string::npos){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+std::string reader(ifstream& text){
+    std::string reader;
+    std::string output;
+    while (getline(text, reader)){
+        std::cout << reader << endl;
+        output.append(reader + "\n");
+    }
+    return output;
+}
+
+void ready(){
+    ifstream dalton("texts/dalton_trumbo.txt");
+    std::string daltonBread = reader(dalton);
+    bool daltonHasBread = hasBread(daltonBread);
+    std::cout << daltonHasBread << endl;
+    
+    // ifstream fyodor("texts/fyodor_dostoevsky.txt");
+    // ifstream kenneth("texts/kenneth_grahame.txt");
+    // ifstream margaret("texts/margaret_atwood.txt");
+    // ifstream terry("texts/terry_pratchett.txt");
+    // ifstream ursula("texts/ursula_leguin.txt");
+    // hasBread(dalton);
+
+}
+
+int main(void)
+{
+    std::cout << "Which mode would you like to run?" << endl;
+    std::cout << "Readymade Bread : 1" << endl;
+    std::cout << "" << endl;
+    std::cout << "" << endl;
+    std::cout << "" << endl;
+    
+    std::string mode = ""; 
+    
+    std::cin >> mode;
+    if (mode == "1"){
+        ready();
+    }
+    
+    return EXIT_SUCCESS;
+}
